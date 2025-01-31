4e56e003d22b7292a55c20fe61de8bd5cb32ad77
Maksym Panov <work.maksym.panov@gmail.com>
2025-01-31 20:05:07 +0200

    Add logging

--- app/controllers/TODOController.scala
+++ app/controllers/TODOController.scala
@@ -2,23 +2,26 @@ package controllers
 
 import com.google.inject.{Inject, Singleton}
 import model.TODO
+import play.api.Logging
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
 import scala.collection.mutable.ListBuffer
 
 @Singleton
-class TODOController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class TODOController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with Logging {
 
   implicit private val todoListJson: OFormat[TODO] = Json.format[TODO]
 
   private val db: ListBuffer[TODO] = ListBuffer[TODO]()
 
-  def getAll: Action[AnyContent] = Action {
+  def getAll: Action[AnyContent] = Action { request =>
+    logger.info(s"getAll: from ${request.remoteAddress}")
     Ok(Json toJson db) as JSON
   }
 
-  def getById(id: Long): Action[AnyContent] = Action {
+  def getById(id: Long): Action[AnyContent] = Action { request =>
+    logger.info(s"getById: requested ID - $id, from ${request.remoteAddress}")
     db.find(_.id == id)
       .map(todo => Ok(Json toJson todo).as(JSON))
       .getOrElse(
@@ -30,11 +33,13 @@ class TODOController @Inject()(val controllerComponents: ControllerComponents) e
 
   def create: Action[TODO] = Action(parse.json[TODO]) { request =>
     val todo = request.body
+    logger.info(s"create: with body - $todo")
     db += todo
     Created(Json.obj("message" -> "TODO created", "todo" -> Json.toJson(todo)))
   }
 
   def delete(id: Long): Action[AnyContent] = Action {
+    logger.info(s"delete: requested ID - $id")
     db.find(_.id == id) match {
       case Some(todo) =>
         db -= todo
